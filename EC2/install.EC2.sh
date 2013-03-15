3a4f727e4860d15d97162e117cce63d738d9ef8e
Brian Speir <no-reply@brianspeir.com>
2013-03-14 18:53:54 -0700

    This script will download the EC2 Bundle and cleanup after itself.

--- EC2/install.EC2.sh
+++ EC2/install.EC2.sh
@@ -7,10 +7,10 @@ sudo -v
 # Install Amazon EC2 API tools as a bundle.
 # aws.amazon.com/developertools/351
 sudo curl -L --create-dirs https://raw.github.com/BrianSpeir/Bundles/master/EC2/Info.plist -o /Library/Bundles/EC2.bundle/Info.plist
-curl -L http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip > /tmp/ec2.zip
-sudo tar -xf /tmp/ec2.zip --strip-components 1 -C /Library/Bundles/EC2.bundle/
+curl -L http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip > /tmp/EC2/bundle.zip
+sudo tar -xf /tmp/EC2/bundle.zip --strip-components 1 -C /Library/Bundles/EC2.bundle/
 sudo chmod -R 755 /Library/Bundles/EC2.bundle
-rm -rf /tmp/ec2.zip
+rm -rf /tmp/EC2/
 
 # Invalidate the user time stamp and require passwords again.
 sudo -k
