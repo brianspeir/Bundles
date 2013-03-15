ea4dcc4642c2370072b509e1644c94446ed94671
Brian Speir <no-reply@brianspeir.com>
2013-03-15 16:44:28 -0700

    Upgraded for Vagrant 1.1.0

--- EC2/install.EC2.sh
+++ EC2/install.EC2.sh
@@ -6,9 +6,10 @@ sudo -v
 
 # Install Amazon EC2 API tools as a bundle.
 # aws.amazon.com/developertools/351
-sudo curl -L --create-dirs https://raw.github.com/BrianSpeir/Bundles/master/EC2/Info.plist -o /Library/Bundles/EC2.bundle/Info.plist
-curl -L http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip > /tmp/EC2/bundle.zip
-sudo tar -xf /tmp/EC2/bundle.zip --strip-components 1 -C /Library/Bundles/EC2.bundle/
+# Version 1.6.7 2013-02-01
+sudo curl https://raw.github.com/BrianSpeir/Bundles/master/EC2/Info.plist --create-dirs -o /Library/Bundles/EC2.bundle/Info.plist
+curl -L http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip --create-dirs -o /tmp/EC2/Install.zip
+sudo tar -xf /tmp/EC2/Install.zip --strip-components 1 -C /Library/Bundles/EC2.bundle/
 sudo chmod -R 755 /Library/Bundles/EC2.bundle
 rm -rf /tmp/EC2/
 
--- Vagrant/install.Vagrant.sh
+++ Vagrant/install.Vagrant.sh
@@ -6,7 +6,7 @@ sudo -v
 
 # Version 1.1.0
 if [ "/Library/Bundles/Vagrant.bundle" = "/Library/Bundles/Vagrant.bundle" ];
-    then sudo rm -rf /Library/Bundles/Vagrant.bundleva
+    then sudo rm -rf /Library/Bundles/Vagrant.bundle
 fi
 curl -L "http://files.vagrantup.com/packages/194948999371e9aee391d13845a0bdeb27e51ac0/Vagrant.dmg" --create-dirs -o /tmp/Vagrant/Install.dmg
 hdiutil attach /tmp/Vagrant/Install.dmg
@@ -15,10 +15,11 @@ hdiutil detach /Volumes/Vagrant
 sudo mv /Applications/Vagrant /Library/Bundles/Vagrant.bundle
 sudo ln -sf /Library/Bundles/Vagrant.bundle/bin/vagrant /usr/bin/vagrant
 sudo curl https://raw.github.com/BrianSpeir/Bundles/master/Vagrant/Info.plist -o /Library/Bundles/Vagrant.bundle/Info.plist
-# Set DEFAULT_HOME = "~/Library/Application Support/Vagrant" to better support practices listed in File System Programing Guide for Mac OS X
-sudo curl https://raw.github.com/BrianSpeir/Bundles/master/Vagrant/environment.rb -o /Library/Bundles/Vagrant.bundle/embedded/gems/gems/vagrant-1.0.6/lib/vagrant/environment.rb
+sudo curl https://raw.github.com/BrianSpeir/Bundles/master/Vagrant/environment.rb -o "/Library/Bundles/Vagrant.bundle/embedded/gems/gems/vagrant-1.1.0/lib/vagrant/environment.rb"
 sudo chmod -R 755 /Library/Bundles/Vagrant.bundle
 rm -rf /tmp/Vagrant/
+vagrant plugin install vagrant-aws
+vagrant plugin install vagrant-rackspace
 
 # Invalidate the user time stamp and require passwords again.
 sudo -k
