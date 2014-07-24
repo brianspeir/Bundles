84c10ac9ef5177a2eecf2fa159bd54d1e1d0d247
Brian Speir <no-reply@brianspeir.com>
2014-07-24 13:04:55 -0700

    Version: 4.2.5

---
+++ bundles/LibreOffice.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#
+#  Copyright (C) 2014 Brian Speir. All rights reserved.
+#
+#  Licensed under The BSD 3-Clause License (the "License"); you may not
+#  use this file except in compliance with the License. You may obtain
+#  a copy of the License at http://opensource.org/licenses/BSD-3-Clause.
+#
+#
+
+#  LibreOffice.sh
+#  Bundles
+#
+#  This script installs LibreOffice for Mac.
+#
+
+
+# Prompt for a password and extend the time stamp.
+sudo -v
+
+# Set installation variables.
+NAME="LibreOffice.app"
+WHERE="/Applications"
+SOURCE="http://download.documentfoundation.org/libreoffice/stable/4.2.5/mac/x86/LibreOffice_4.2.5_MacOS_x86.dmg"
+
+# Remove existing installation.
+if [ -s "$WHERE"/"$NAME" ]; then
+    sudo rm -rf "$WHERE"/"$NAME"
+fi
+
+# Install.
+echo "Downloading..."
+curl -L# "$SOURCE" -o ${TMPFILE=$(mktemp -t tmp)}
+echo "Installing..."
+hdiutil attach "$TMPFILE" -nobrowse -quiet -mountpoint ${TMPVOL=/Volumes/$(basename $TMPFILE)}
+sudo cp -R "$TMPVOL"/"$NAME" "$WHERE"/
+hdiutil detach -quiet "$TMPVOL"
+rm "$TMPFILE"
+
+echo "Finished."
