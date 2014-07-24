3d9afda1fc9dfc84daf26848ac8986dc51fc9e7c
Brian Speir <no-reply@brianspeir.com>
2014-07-24 13:06:45 -0700

    Version: 1.9.6

---
+++ bundles/SourceTree.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#
+#  Copyright (C) 2014 Brian Speir. All rights reserved.
+#
+#  Licensed under The BSD 3-Clause License (the "License"); you may not
+#  use this file except in compliance with the License. You may obtain
+#  a copy of the License at http://opensource.org/licenses/BSD-3-Clause.
+#
+#
+
+#  SourceTree.sh
+#  Bundles
+#
+#  This script installs SourceTree for Mac.
+#
+
+
+# Prompt for a password and extend the time stamp.
+sudo -v
+
+# Set installation variables.
+NAME="SourceTree.app"
+WHERE="/Applications"
+SOURCE="http://downloads.atlassian.com/software/sourcetree/SourceTree_1.9.6.dmg"
+
+# Remove existing installation.
+if [ -s "$WHERE"/"$NAME" ]; then
+    sudo rm -rf "$WHERE"/"$NAME"
+fi
+
+# Install.
+echo "Downloading..."
+curl -L# "$SOURCE" -o ${TMPFILE=$(mktemp -t tmp)}
+echo "Installing..."
+hdiutil attach "$TMPFILE" -nobrowse -quiet -mountpoint ${TMPVOL=/Volumes/$(basename $TMPFILE)}
+sudo cp -R "$TMPVOL"/"$NAME" "$WHERE"/
+hdiutil detach -quiet "$TMPVOL"
+rm "$TMPFILE"
+
+echo "Finished."
